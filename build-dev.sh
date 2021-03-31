132a8bf387120553dc905df504a7181abb41f3a2
yarnin-p <55054562+yarnin-p@users.noreply.github.com>
2021-03-31 19:05:14 +0700

    Create build-dev.sh

---
+++ build-dev.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+sudo rm -rf /var/www/html/nutritions
+sudo cp -r /var/lib/jenkins/workspace/nutritions /var/www/html/nutritions
+cd /var/www/html/nutritions
+sudo npm install
+sudo npm run build
+
